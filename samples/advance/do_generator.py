03df3588f95d37ccb95fbce745264f6786c7a51f
arronwelch <Davego@protonmail.com>
2021-03-01 19:28:39 +0800

    .

--- samples/advance/do_generator.py
+++ samples/advance/do_generator.py
@@ -56,3 +56,20 @@ while True:
         print('Generator return value:',e.value)
         break
 
+# 练习
+# 杨辉三角定义如下：
+#           1
+#          / \
+#         1   1
+#        / \ / \
+#       1   2   1
+#      / \ / \ / \
+#     1   3   3   1
+#    / \ / \ / \ / \
+#   1   4   6   4   1
+#  / \ / \ / \ / \ / \
+# 1   5   10  10  5   1
+# 把每一行看做一个list，试写一个generator，不断输出下一行的list：
+
+def triangles():
+    list
