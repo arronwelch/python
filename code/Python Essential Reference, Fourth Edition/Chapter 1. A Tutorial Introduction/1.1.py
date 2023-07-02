a59541ab586cc14e59d11296399182c936e6de18
nick <Davego@protonmail.com>
2023-07-02 23:01:45 +0800

    restart learning python

---
+++ code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Running Python
+
+# in python2
+# print "Hello World"
+
+# in python3
+# python3 helloworld.py
+print("Hello World")
+
+# use interactive Python as their desktop calculator.
+# >>> 6000 + 4523.50 + 134.12
+# 10657.620000000001
+# >>> _ + 8192.32
+# 18849.940000000002
+# >>> raise SystemExit()
+
