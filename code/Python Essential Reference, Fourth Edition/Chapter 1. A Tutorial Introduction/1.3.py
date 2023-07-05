d66725149e25471284c294d76122dabba8258389
nick <Davego@protonmail.com>
2023-07-05 23:58:05 +0800

    indentation use four spaces and formatting print

--- code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.3.py
+++ code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.3.py
@@ -7,7 +7,30 @@
 principal = 1000; rate = 0.05; numyears = 5;
 year = 1
 while year <= numyears:
-	principal = principal * (1 + rate)
-	print(year, principal) # Remider: print(year, principal) in Python 3
-	year += 1
+    principal = principal * (1 + rate)
+    print (year, principal) # Remider: print(year, principal) in Python 3
+    year += 1
+print ("-----")
 
+principal = 1000; rate = 0.05; numyears = 5;
+year = 1
+while year <= numyears:
+    principal = principal * (1 + rate)
+    print ("%3d %0.2f" % (year, principal)) # Python 3
+    year += 1
+print ("-----")
+
+principal = 1000; rate = 0.05; numyears = 5;
+year = 1
+while year <= numyears:
+    principal = principal * (1 + rate)
+    print (format(year,"3d"),format(principal,"0.2f")) # Python 3
+    year += 1
+print ("-----")
+
+principal = 1000; rate = 0.05; numyears = 5;
+year = 1
+while year <= numyears:
+    principal = principal * (1 + rate)
+    print ("{0:3d} {1:0.2f}".format(year,principal)) # Python 3
+    year += 1
