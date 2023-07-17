03b4906d1b28e4c73751b40cd865215625fd16b3
nick <Davego@protonmail.com>
2023-07-18 00:55:08 +0800

    print() to file and write() to file

--- code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.2.py
+++ code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.2.py
@@ -4,30 +4,32 @@
 
 # python3 1.2.py
 # Listing 1.1 Simple Compound-Interest Calculation
+
+print ("1-----")
 principal = 1000; rate = 0.05; numyears = 5;
 year = 1
 while year <= numyears:
     principal = principal * (1 + rate)
     print (year, principal) # Remider: print(year, principal) in Python 3
     year += 1
-print ("-----")
 
+print ("2-----")
 principal = 1000; rate = 0.05; numyears = 5;
 year = 1
 while year <= numyears:
     principal = principal * (1 + rate)
     print ("%3d %0.2f" % (year, principal)) # Python 3
     year += 1
-print ("-----")
 
+print ("3-----")
 principal = 1000; rate = 0.05; numyears = 5;
 year = 1
 while year <= numyears:
     principal = principal * (1 + rate)
     print (format(year,"3d"),format(principal,"0.2f")) # Python 3
     year += 1
-print ("-----")
 
+print ("4-----")
 principal = 1000; rate = 0.05; numyears = 5;
 year = 1
 while year <= numyears:
--- code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.4.py
+++ code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.4.py
@@ -5,10 +5,38 @@
 # $ echo "the first line" > foo.txt
 # $ echo "the second line" >> foo.txt
 # python3 1.4.py
-f = open("foo.txt") # Returns a file object
-line = f.readline() # Invokes readline() method on file
+f = open("foo.txt")     # Returns a file object
+line = f.readline()     # Invokes readline() method on file
 while line:
-    # print line,      # trailing ',' omits newline character
-    print(line,end='') # Use in Python 3
+    # print line,       # trailing ',' omits newline character
+    print(line,end='')  # Use in Python 3
     line = f.readline()
 f.close
+
+# Python provides a dedicated statement: for
+for line in open("foo.txt"):
+    print(line,end='')
+
+principal = 1000    # Initial amount
+rate = 0.05         # Interest rate
+numyears = 5        # Number of years
+year = 1
+f = open("out1", "w")    # Open a file for writing
+while year <= numyears:
+    principal = principal * (1 + rate)
+    #print >>f, "%3d %0.2f" % (year,principal)
+    print("%3d %0.2f" % (year,principal), file=f)
+    year += 1
+f.close()
+
+principal = 1000    # Initial amount
+rate = 0.05         # Interest rate
+numyears = 5        # Number of years
+year = 1
+f = open("out2", "w")    # Open a file for writing
+while year <= numyears:
+    principal = principal * (1 + rate)
+    #print >>f, "%3d %0.2f" % (year,principal)
+    f.write("%3d %0.2f\n" % (year,principal))
+    year += 1
+f.close()
