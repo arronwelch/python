d9b641f6067c07296fb1d06f5431e3b79798b9c8
nick <Davego@protonmail.com>
2023-07-29 23:59:29 +0800

    extract or reassign a portion of a list by using the slicing operator

--- code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.6.py
+++ code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.6.py
@@ -16,3 +16,16 @@ print(names)
 
 names.insert(2, "Thomas")
 print(names)
+
+b = names[0:2]  # Returns [ "Jeff", "Mark" ]
+print(b)
+
+c = names[2:]   # Returns [ "Thomas", "Ann", "Phil", "Paula" ]
+print(c)
+
+names[1] = 'Jeff'   # Replace the 2nd item in names with 'Jeff'
+print(names)
+
+names[0:2] = ['Dave', 'Mark', 'Jeff']   # Replace the first two items of
+                                        # the list with the list on the right.
+print(names)
