9d0a582e5c9bb1102353513b51f389b40f1df412
nick <Davego@protonmail.com>
2023-08-03 00:00:13 +0800

    tuples

---
+++ code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.7.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 1.7 Tuples
+
+stock = ('GOOG', 100, 490.10)
+address = ('www.python.org', 80)
+person = (first_name, last_name, phone)
+
+print(stock)
+print(address)
+print(person)
