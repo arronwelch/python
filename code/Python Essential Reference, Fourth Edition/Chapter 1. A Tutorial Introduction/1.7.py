132e258d1280226938936d689d7049bd2a49e855
nick <Davego@protonmail.com>
2023-08-04 23:58:22 +0800

    python recognizes that a tuple is intended

--- code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.7.py
+++ code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.7.py
@@ -2,10 +2,27 @@
 # -*- coding: utf-8 -*-
 # 1.7 Tuples
 
-stock = ('GOOG', 100, 490.10)
-address = ('www.python.org', 80)
-person = (first_name, last_name, phone)
+first_name = "Bob"
+last_name = "Bill"
+phone = 111222
+
+# stock = ('GOOG', 100, 490.10)
+# address = ('www.python.org', 80)
+# person = (first_name, last_name, phone)
+
+stock = 'GOOG', 100, 490.10
+address = 'www.python.org', 80
+person = first_name, last_name, phone
 
 print(stock)
 print(address)
 print(person)
+
+item = "item"
+a = ()      # 0-tuple (empty tuple)
+b = (item,) # 1-tuple (note that trailing comma)
+c = item,   # 1-tuple (note that trailing comma)
+
+print(a)
+print(b)
+print(c)
