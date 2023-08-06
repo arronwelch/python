71eddd02da69cd4d46fa288976888143309f8fb9
nick <Davego@protonmail.com>
2023-08-06 23:57:26 +0800

    tuples are immutable

--- code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.7.py
+++ code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.7.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # 1.7 Tuples
 
-first_name = "Bob"
-last_name = "Bill"
-phone = 111222
+first_name = "Robert"
+last_name = "Smith"
+phone = 123456
 
 # stock = ('GOOG', 100, 490.10)
 # address = ('www.python.org', 80)
@@ -36,7 +36,20 @@ host, port = address
 print(host)
 print(port)
 
-first_name, last_name, phone = person
-print(first_name)
-print(last_name)
-print(phone)
+first_name1, last_name1, phone1 = person
+print(first_name1)
+print(last_name1)
+print(phone1)
+
+# File containing lines of the form "name,shares,price"
+filename = "portfolio.csv"
+portfolio = []
+for line in open(filename):
+    fields = line.split(",")    # Split each line into a list
+    name = fields[0]            # Extract and convert individual fields
+    shares = int(fields[1])
+    price = float(fields[2])
+    stock = (name,shares,price) # Create a tuple (name, shares, price)
+    portfolio.append(stock)     # Append to list of records
+    print(portfolio)
+
