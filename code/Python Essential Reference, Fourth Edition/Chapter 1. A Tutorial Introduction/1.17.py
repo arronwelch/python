c7c5a282a17d2f0a108d4046af2757b041299794
nick <Davego@protonmail.com>
2023-08-24 00:40:32 +0800

    help(),  __doc__ and pydoc

---
+++ code/Python Essential Reference, Fourth Edition/Chapter 1. A Tutorial Introduction/1.17.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 1.17 Getting Help
+
+# in interactive mode, you can use the help() command to get
+# information about built-in modules and other aspects of Python.
+
+# >>> print(issubclass.__doc__)
+
+# >>> import pydoc
+# >>> help(pydoc)
+
+# >>> import string
+# >>> help(string)
