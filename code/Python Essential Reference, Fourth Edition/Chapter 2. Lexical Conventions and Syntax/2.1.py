dd6e86b46fd6886811c4a79a98f33c3823029455
nick <Davego@protonmail.com>
2023-08-26 23:58:27 +0800

    basic Line Structure and Indentation

--- code/Python Essential Reference, Fourth Edition/Chapter 2. Lexical Conventions and Syntax/2.1.py
+++ code/Python Essential Reference, Fourth Edition/Chapter 2. Lexical Conventions and Syntax/2.1.py
@@ -6,7 +6,39 @@ import math
 x = 1.2
 y = 2.3
 n = 3.4
+# line-continuation character : (\)
 a = math.cos(3 * (x - n)) + \
     math.sin(3 * (y - n))
 
 print("a = ",a)
+
+# Indentation must be consistent
+# if a:
+#     statement1  # Consistent indentation
+#     statement2
+# else:
+#     statement3
+#         statement4  # Inconsistent indentation (error)
+
+# body can be placed on the same line
+# if a:   statement1
+# else:   statement2
+
+# use pass statement to denote an empty body or block
+# if a:
+#     pass
+# else:
+#     statements
+
+# use of spaces is universally preferred(and encouraged)
+
+# Running Python with the -t option prints warning messages when
+# tabs and spaces are mixed inconsistently within the same program block.
+
+# The -tt option turns these warning messages into TabError exceptions.
+
+# To place more than one statement on a line, separate the statements with
+# a semi-conlon(;).
+
+# The # character denotes a comment
+
