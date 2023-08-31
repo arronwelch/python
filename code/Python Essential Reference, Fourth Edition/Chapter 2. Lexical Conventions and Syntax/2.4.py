fac00a4696686d1d34d99393ef004ff80df68393
nick <Davego@protonmail.com>
2023-08-31 23:58:52 +0800

    String Literals

---
+++ code/Python Essential Reference, Fourth Edition/Chapter 2. Lexical Conventions and Syntax/2.4.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 2.4 String Literals
+
+# String literals are used to specify a sequence of characters and defined by enclosing
+# text in single('),double("),or triple('''or""") quotes.There is no semantic difference
+# between quoting styles other than the requirement that you use the same type of quote to
+# start and terminate a string.
+
+# Single- and double-quoted strings must be defined on a single line, whereas triple-quoted
+# strings can span multiple lines and include all of the enclosed formatting(that is,newlines,
+# tabs,spaces,and so on).
+
+# Adjacent strings (separated by white space,newline,or a line-continuation character) such as
+# "hello" 'world' are concatenated to form a single string "helloworld".
+
