b50d8b8948987676502e998c17a98235b27506f0
nick <Davego@protonmail.com>
2023-09-06 23:49:12 +0800

    Containers

---
+++ code/Python Essential Reference, Fourth Edition/Chapter 2. Lexical Conventions and Syntax/2.5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 2.5 Containers
+
+a = [ 1, 3.4, 'hello' ] # A list
+b = ( 10, 20, 30 )      # A tuple
+c = { 'a':3,'b':42}     # A dictionary
+
+print('a = ', a)
+print('b = ', b)
+print('c = ', c)
