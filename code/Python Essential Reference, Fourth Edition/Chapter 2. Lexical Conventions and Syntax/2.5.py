d7fb204700421bddd7bd8af4d5f6ec6ffe4cc9d5
nick <Davego@protonmail.com>
2023-09-08 22:01:11 +0800

    no use line-continuation character(\)

--- code/Python Essential Reference, Fourth Edition/Chapter 2. Lexical Conventions and Syntax/2.5.py
+++ code/Python Essential Reference, Fourth Edition/Chapter 2. Lexical Conventions and Syntax/2.5.py
@@ -10,3 +10,11 @@ c = { 'a':3,'b':42}     # A dictionary
 print('a = ', a)
 print('b = ', b)
 print('c = ', c)
+
+d = [ 1,
+      3.4,
+      'hello',
+    ]
+print('d = ',d)
+# line-continuation character(\)
+# trailing commat
