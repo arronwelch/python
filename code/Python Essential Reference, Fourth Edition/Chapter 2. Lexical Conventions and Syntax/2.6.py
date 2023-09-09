8abe43d7c9562583fe0ada23a6e77992f6badcb8
nick <Davego@protonmail.com>
2023-09-09 20:32:33 +0800

    operators, delimiters, and special symbols

---
+++ code/Python Essential Reference, Fourth Edition/Chapter 2. Lexical Conventions and Syntax/2.6.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 2.6 Operators, Delimiters, and Special Symbols
+
+# Operators:
+# + - * ** / // % << >> & |
+# ^ ~ < >  <= >= == != <> +=
+# -= *= /= //= %= **= &= |= ^= >>= <<=
+
+# delimiters
+# ( ) [ ] { } , : . ` = ;
+
+# equal(=) character serves as a delimiter
+# comma(,) character is used to delimit arguments to a function
+# period(.) is also used in floating-point numbers
+# ellipsis(...) used in extended slicing operations.
+
+# special symbols
+# ' " # \ @
