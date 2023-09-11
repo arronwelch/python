ac606c64bfc805c73e14c164fdb52fbc982f0a03
nick <Davego@protonmail.com>
2023-09-11 21:44:45 +0800

    decorators

---
+++ code/Python Essential Reference, Fourth Edition/Chapter 2. Lexical Conventions and Syntax/2.8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 2.8 Decorators
+
+class Foo(object):
+    @staticmethod
+    def bar():
+        pass
+    @foo
+    @bar
+    def spam():
+        pass
+
+# More information about decorators can be found in Chapter 6,
+# "Functions and Functional Programming", and Chapter 7,"Classes and
+# Object-Oriented Programming".
