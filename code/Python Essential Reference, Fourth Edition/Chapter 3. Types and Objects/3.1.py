1ccf95f4639d5339673222daefd3fa89ee9df104
nick <Davego@protonmail.com>
2023-09-13 00:28:01 +0800

    identity, type, class instance, mutable, immutable, container or collection, attributes and methods, dot(.) operator

---
+++ code/Python Essential Reference, Fourth Edition/Chapter 3. Types and Objects/3.1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 3.1 Terminology
+
+a = 3 + 4j      # Create a complex number
+r = a.real      # Get the real part (an attribute)
+print('r = ', r)
+
+b = [1, 2, 3]   # Create a list
+b.append(7)     # Add a new element using the append method
+print('b = ', b)
+
