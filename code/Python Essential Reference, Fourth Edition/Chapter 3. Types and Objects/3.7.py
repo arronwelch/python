f50808d906a6268598407d42f1b9cb77dbfaa792
nick <Davego@protonmail.com>
2023-10-23 00:16:35 +0800

    all() and any()

--- code/Python Essential Reference, Fourth Edition/Chapter 3. Types and Objects/3.7.py
+++ code/Python Essential Reference, Fourth Edition/Chapter 3. Types and Objects/3.7.py
@@ -9,13 +9,35 @@
 # tion, deletion, and substitution of elements. All sequences support iteration.
 
 # Operations Common to All Sequences
-# s[i]
+
+# s[i] | Returns element i of a sequence
 s = 'abcdefg'
 print('s = ',s)
 print('s[2] = ',s[2])
-# s[i:j]
+
+# s[i:j] | Returns a slice
 print('s[2:3] = ',s[2:3])
 print('s[2:4] = ',s[2:4])
 
-# s[i:j:stride]
+# s[i:j:stride] | Returns an extended slice
 print('s[2::2] = ',s[2::2])
+
+# len(s) | Number of elements in s
+print('len(s) = ', len(s))
+
+# min(s) | Minimum value in s
+print('min(s) = ', min(s))
+
+# max(s) | Maximum value in s
+print('max(s) = ', max(s))
+
+s = [1,2,3,4,5,6,7]
+print('s = ',s)
+# sum(s [, initial]) | Sum of items in s
+print('sum(s) = ', sum(s))
+
+# all(s) | Checks whether all items in s are True.
+print('all(s) = ', all(s))
+
+# any(s) | Checks whether any item in s is True.
+print('any(s) = ', any(s))
