e92cad6c4806f1bf6e1b1fed5172981cf60742a3
nick <Davego@protonmail.com>
2023-11-01 01:45:43 +0800

    sequence types: lists -> converts s to a list

---
+++ code/Python Essential Reference, Fourth Edition/Chapter 3. Types and Objects/3.8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 3.8 Sequence Types:Lists
+
+# list(s) | Converts s to a list
+s = 'abcd'
+print('s =',s,type(s))
+l = list(s)
+print('l =',l,type(l))
+
+
