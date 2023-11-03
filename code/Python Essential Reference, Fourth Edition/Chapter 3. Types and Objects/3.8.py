834d69acbda8fec6f314fee9776322b1ab1f1432
nick <Davego@protonmail.com>
2023-11-03 22:37:01 +0800

    extend() and count() method of lists

--- code/Python Essential Reference, Fourth Edition/Chapter 3. Types and Objects/3.8.py
+++ code/Python Essential Reference, Fourth Edition/Chapter 3. Types and Objects/3.8.py
@@ -3,12 +3,21 @@
 
 # 3.8 Sequence Types:Lists
 
-# list(s) | Converts s to a list
+# list(s) | Converts s to a list.
 s = 'abcd'
 print('s =',s,type(s))
 l = list(s)
 print('l =',l,type(l))
 
-# s.append(x) | Appends a new element,x,to the end of s
-l.append('f')
+# s.append(x) | Appends a new element,x,to the end of s.
+l.append('e')
 print('l =',l,type(l))
+
+# s.extend(t) | Appends a new list,t,to the end of s.
+l.extend(list('fg'))
+l.extend('hi')
+print('l =',l,type(l))
+
+# s.count(x) | Counts occurrences of x in s.
+print('l.count(\'i\')=',l.count('i'))
+print('l.count(\'j\')=',l.count('j'))
