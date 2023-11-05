a7996cbda4a5c21d48d568f893d067c6ac148fbd
nick <Davego@protonmail.com>
2023-11-05 22:15:07 +0800

    list methods: index,insert,pop,remove,reverse,sort

--- code/Python Essential Reference, Fourth Edition/Chapter 3. Types and Objects/3.8.py
+++ code/Python Essential Reference, Fourth Edition/Chapter 3. Types and Objects/3.8.py
@@ -21,3 +21,41 @@ print('l =',l,type(l))
 # s.count(x) | Counts occurrences of x in s.
 print('l.count(\'i\')=',l.count('i'))
 print('l.count(\'j\')=',l.count('j'))
+
+# s.index(x [,start [,stop]]) | Returns the smallest i where s[i] == x. start
+#                               and stop optionally specify the starting and ending
+#                               index for the search.
+print('l.index(\'c\',0,4) =',l.index('c',0,4),type(l.index('c',0,4)))
+
+# s.insert(i,x) | Inserts x at index i.
+l.insert(4,'I')
+print('l.insert(4,\'I\')')
+print('l =',l,type(l))
+
+# s.pop([i]) | Returns the element i and removes it from the
+#              list. If i is omitted, the last element is returned.
+l.pop()
+print('l.pop()')
+print('l =',l,type(l))
+l.pop(5)
+print('l.pop(5)')
+print('l =',l,type(l))
+
+l.append('I')
+print('l.append(\'I\')')
+print('l =',l,type(l))
+# s.remove(x) | Searches for x and removes the smallest index's it from s.
+l.remove('I')
+print('l =',l,type(l))
+
+# s.reverse() | Reverses items of s in place.
+l.reverse() 
+print('l =',l,type(l))
+
+# s.sort([key [, reverse]]) | Sorts items of s in place. key is a key function.
+#                           | reverse is a flag that sorts the list in reverse
+#                           | order. key and reverse should always be specified
+#                           | as keyword arguments.
+l.sort()
+print('l =',l,type(l))
+
